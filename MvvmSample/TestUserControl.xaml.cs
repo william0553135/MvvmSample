aa836bc99fc94f46474bae6f95587b9f3bac8b0f
william0553135 <william0553135@gmail.com>
2023-10-13 12:59:30 +0800

    + 初版

---
+++ MvvmSample/TestUserControl.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NV
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+    public class TestViewModel
+    {
+        public int MinInterval { get; set; }
+        public int MinVolume { get; set; }
+
+        public List<TestClass> TestClassList { get; set; }
+        public ObservableCollection<TestClass> TestClassObservableCollection { get; set; }
+        public List<TestClassWithNotifyPropertyChanged> TestClassListWithNotifyPropertyChanged { get; set; }
+        public ObservableCollection<TestClassWithNotifyPropertyChanged> TestClassObservableCollectionWithNotifyPropertyChanged { get; set; }
+
+        public DateTime SelectedDate { get; set; }
+        public ICommand AddCommand { get; }
+        public ICommand PropertyChangeCommand { get; }
+
+        public TestViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddClick);
+            PropertyChangeCommand = new RelayCommand(OnPropertyChangeClick);
+
+            MinInterval = 5;
+            MinVolume = 30;
+            SelectedDate = DateTime.Today;
+
+            TestClassList = new List<TestClass>()
+            {
+                new TestClass() { Id = 1, Name = "William" },
+                new TestClass() { Id = 2, Name = "John" },
+                new TestClass() { Id = 3, Name = "Mary" }
+            };
+
+            TestClassObservableCollection = new ObservableCollection<TestClass>()
+            {
+                new TestClass() { Id = 1, Name = "William" },
+                new TestClass() { Id = 2, Name = "John" },
+                new TestClass() { Id = 3, Name = "Mary" }
+            };
+
+            TestClassListWithNotifyPropertyChanged = new List<TestClassWithNotifyPropertyChanged>()
+            {
+                new TestClassWithNotifyPropertyChanged() { Id = 1, Name = "William" },
+                new TestClassWithNotifyPropertyChanged() { Id = 2, Name = "John" },
+                new TestClassWithNotifyPropertyChanged() { Id = 3, Name = "Mary" }
+            };
+
+            TestClassObservableCollectionWithNotifyPropertyChanged = new ObservableCollection<TestClassWithNotifyPropertyChanged>()
+            {
+                new TestClassWithNotifyPropertyChanged() { Id = 1, Name = "William" },
+                new TestClassWithNotifyPropertyChanged() { Id = 2, Name = "John" },
+                new TestClassWithNotifyPropertyChanged() { Id = 3, Name = "Mary" }
+            };
+
+        }
+
+        private void OnPropertyChangeClick()
+        {
+            TestClassList[0].Name = "Changed";
+            TestClassObservableCollection[0].Name = "Changed";
+            TestClassListWithNotifyPropertyChanged[0].Name = "Changed";
+            TestClassObservableCollectionWithNotifyPropertyChanged[0].Name = "Changed";
+        }
+
+        private void OnAddClick()
+        {
+            var testClass = new TestClass() { Id = 4, Name = "TestAdded" };
+            TestClassList.Add(testClass);
+            TestClassObservableCollection.Add(testClass);
+
+            var testClassWithNotifyPropertyChanged = new TestClassWithNotifyPropertyChanged() { Id = 4, Name = "TestAdded" };
+            TestClassListWithNotifyPropertyChanged.Add(testClassWithNotifyPropertyChanged);
+            TestClassObservableCollectionWithNotifyPropertyChanged.Add(testClassWithNotifyPropertyChanged);
+        }
+    }
+
+    public class TestClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TestClassWithNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _name;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value == _id) return;
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MvvmSample/Util.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NV
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Func<Task> _asyncExecute;
+        private readonly Action _syncExecute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Func<Task> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Func<Task> asyncExecute, Func<bool> canExecute)
+        {
+            _asyncExecute = asyncExecute ?? throw new ArgumentNullException(nameof(asyncExecute));
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action syncExecute, Func<bool> canExecute)
+        {
+            _syncExecute = syncExecute ?? throw new ArgumentNullException(nameof(syncExecute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            if (_asyncExecute != null)
+            {
+                await _asyncExecute();
+            }
+            else if (_syncExecute != null)
+            {
+                _syncExecute();
+            }
+        }
+    }
+
+    public class TickHelper
+    {
+        // 整合了TickUp和TickDn的功能
+        public static decimal Tick(decimal price, bool isUp)
+        {
+            if (price >= 1000 || (!isUp && price > 1000))
+                return isUp ? 5 : -5;
+            else if (price >= 500 || (!isUp && price > 500))
+                return isUp ? 1 : -1;
+            else if (price >= 100 || (!isUp && price > 100))
+                return isUp ? 0.5m : -0.5m;
+            else if (price >= 50 || (!isUp && price > 50))
+                return isUp ? 0.1m : -0.1m;
+            else if (price >= 10 || (!isUp && price > 10))
+                return isUp ? 0.05m : -0.05m;
+            else
+                return isUp ? 0.01m : -0.01m;
+        }
+
+        // 判斷了tick的值，避免了整數溢出
+        public static decimal Tick(decimal price, int tick, bool isUp)
+        {
+            decimal new_price = price;
+            if (tick > 0)
+            {
+                for (int i = 0; i < tick; i++)
+                    new_price += Tick(new_price, isUp);
+            }
+            return new_price;
+        }
+
+        public static decimal AdjustPriceToTick(decimal price)
+        {
+            // 獲取較大的TickSize
+            decimal tickSize = Tick(price, true);
+
+            // 將價格調整到最接近的有效單位
+            decimal adjustedPrice = Math.Ceiling(price / tickSize) * tickSize;
+
+            return adjustedPrice;
+        }
+    }
+}
